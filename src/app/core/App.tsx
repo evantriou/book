7234786e8a0a94a52979af8d27d7db1beec3f09b
Evan <triouevan@gmmail.com>
2023-10-01 09:55:00 +0200

    style comming

--- src/app/core/App.tsx
+++ src/app/core/App.tsx
@@ -1,9 +1,5 @@
-import { useRef } from 'react';
-import { useState } from 'react';
 import './App.css';
 import Header from './Header';
-import SimulationPopup from './../simulations/SimulationPopup';
-import { SimuEngine } from './../simulations/SimuEngine';
 import { Route, Routes } from 'react-router-dom';
 import MainPage from './book/MainPage';
 import Contact from './contact/Contact';
@@ -12,7 +8,7 @@ function App() {
 	return (
 		<div className="App">
 			<Header />
-			<main className="App-main">
+			<main className="AppMain">
 				<Routes>
 					<Route path="/" element={<MainPage />} />
 					<Route path="/book" element={<MainPage />} />
--- src/app/core/Header.tsx
+++ src/app/core/Header.tsx
@@ -3,12 +3,11 @@ import Navbar from 'react-bootstrap/Navbar';
 
 function Header() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary" style={{ boxShadow: '0px 0px 15px rgba(192, 192, 192, 0.5)' }}>
+    <Navbar expand="lg" className="Header">
       <Container>
-        <Navbar.Brand href="/book" style = {{justifyContent: 'left'}}>Book</Navbar.Brand>
-        <Navbar.Brand href="/contact" style = {{justifyContent: 'left'}}>Contact</Navbar.Brand>
-        <Navbar.Brand href="/commingNext" style = {{justifyContent: 'left'}}>Comming next</Navbar.Brand>
-        <Navbar.Text>Triou Evan</Navbar.Text>
+        <Navbar.Brand href="/book" className="HeaderEntry">Book</Navbar.Brand>
+        <Navbar.Brand href="/contact" className="HeaderEntry">Contact</Navbar.Brand>
+        <Navbar.Text className="HeaderEntryName">Triou Evan</Navbar.Text>
       </Container>
     </Navbar>
   );
