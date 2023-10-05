98c362ec8d8a36b440850afef1a41f1b9e0104c6
Evan <triouevan@gmmail.com>
2023-10-05 08:08:26 +0200

    nice try

--- src/app/simulations/SimulationPopup.tsx
+++ src/app/simulations/SimulationPopup.tsx
@@ -135,33 +135,89 @@ function SimulationPopup({ selectedSimulation, closePopup }: SimulationPopupProp
 }
 
 function adaptCanvas(canvas: HTMLCanvasElement, canvasContainer: HTMLElement, ctx: CanvasRenderingContext2D): void {
+
+    // const initialCanvasWidth: number = canvas.width;
+    // const initialCanvasHeight: number = canvas.height;
+
+    // const aspectRatio = initialCanvasWidth / initialCanvasHeight;
+
+    // const containerWidth = canvasContainer.clientWidth;
+    // const containerHeight = canvasContainer.clientHeight;
+
+    // let newWidth: number = 0;
+    // let newHeight: number = 0;
+
+    // if (containerWidth / aspectRatio <= containerHeight) {
+    //     // Fit based on width
+    //     newWidth = containerWidth;
+    //     newHeight = containerWidth / aspectRatio;
+    // } else {
+    //     // Fit based on height
+    //     newHeight = containerHeight;
+    //     newWidth = containerHeight * aspectRatio;
+    // }
+
+    // canvas.style.width = newWidth+"px";
+    // canvas.style.height = newHeight+"px";
+
+    // const scalingFactorWidth: number = newWidth / initialCanvasWidth;
+    // const scalingFactorHeight: number = newHeight / initialCanvasHeight;
+
+    // ctx.scale(scalingFactorWidth, scalingFactorHeight);
+
+
     // Calculate the canvas dimensions based on the parent's dimensions
     const containerWidth = canvasContainer.clientWidth;
     const containerHeight = canvasContainer.clientHeight;
 
+    console.log("canvas.clientWidth ", canvas.clientWidth)
+    console.log("canvas.clientHeight ", canvas.clientHeight)
+
+    const initialCanvasWidth: number = canvas.clientWidth;
+    const initialCanvasHeight: number = canvas.clientHeight;
+
     canvas.style.width = "100%";
     canvas.style.height = "100%";
 
-    // Set the canvas dimensions to fit the container while maintaining the aspect ratio
-    const aspectRatio = canvas.width / canvas.height;
-    if (containerWidth / aspectRatio <= containerHeight) {
-        // Fit based on width
-        canvas.width = containerWidth;
-        canvas.height = containerWidth / aspectRatio;
-    } else {
-        // Fit based on height
-        canvas.height = containerHeight;
-        canvas.width = containerHeight * aspectRatio;
-    }
+    let newWidth: number = canvas.clientWidth;
+    let newHeight: number = canvas.clientHeight;
 
-    // ------------------------------
-    // Set actual size in memory (scaled to account for extra pixel density).
-    // var scale = window.devicePixelRatio;
-    // canvas.width = containerWidth * scale;
-    // canvas.height = containerHeight * scale;
+    console.log("canvas.clientWidth ", canvas.clientWidth)
+    console.log("canvas.clientHeight ", canvas.clientHeight)
 
-    // Normalize coordinate system to use css pixels.
-    ctx.scale(aspectRatio, aspectRatio);
+    canvas.width = newWidth;
+    canvas.height = newHeight;
+
+    const scalingFactorWidth: number = newWidth / initialCanvasWidth;
+    const scalingFactorHeight: number = newHeight / initialCanvasHeight;
+
+    ctx.scale(scalingFactorWidth, scalingFactorHeight);
+
+
+    // console.log("canvas.style.width ",  canvas.style.width)
+    // console.log(" canvas.style.height ",  canvas.style.height)
+
+
+    // // Set the canvas dimensions to fit the container while maintaining the aspect ratio
+    // const aspectRatio = canvas.width / canvas.height;
+    // if (containerWidth / aspectRatio <= containerHeight) {
+    //     // Fit based on width
+    //     canvas.width = containerWidth;
+    //     canvas.height = containerWidth / aspectRatio;
+    // } else {
+    //     // Fit based on height
+    //     canvas.height = containerHeight;
+    //     canvas.width = containerHeight * aspectRatio;
+    // }
+
+    // canvas.width = canvas.width * window.devicePixelRatio;
+    // canvas.height = canvas.height * window.devicePixelRatio;
+
+    // // Normalize coordinate system to use css pixels.
+    // ctx.scale(window.devicePixelRatio, window.devicePixelRatio);
+
+    // console.log("canvas real w ", canvas.width)
+    // console.log("canvas real h ", canvas.height)
 
     return;
 }
