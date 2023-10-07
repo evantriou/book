46ddb207f714602d203617c21daa9d59b2e9f84d
Evan <triouevan@gmmail.com>
2023-10-07 11:04:14 +0200

    changes

--- src/app/simulations/SimulationPopup.tsx
+++ src/app/simulations/SimulationPopup.tsx
@@ -1,9 +1,25 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Modal } from 'react-bootstrap';
 import 'rc-slider/assets/index.css';
 import { SimuEngine } from './SimuEngine';
-import SimulationCanvas from './SimulationCanvas';
 import { Toolbar } from './Toolbar';
+import { useRef } from "react";
+import { SimuEngineBoids } from "./boids/SimuEngineBoids";
+import { ToolbarBoids } from "./boids/ToolbarBoids";
+import { SimuEngineDLA } from "./dla/SimuEngineDLA";
+import { ToolbarDLA } from "./dla/ToolbarDLA";
+import { SimuEngineFractal } from "./fractal/SimuEngineFractal";
+import { ToolbarFractal } from "./fractal/ToolbarFractal";
+import { SimuEngineGOL } from "./gol/SimuEngineGOL";
+import { ToolbarGOL } from "./gol/ToolbarGOL";
+import { SimuEnginePaths } from "./paths/SimuEnginePaths";
+import { ToolbarPaths } from "./paths/ToolbarPaths";
+import { SimuEnginePerlin } from "./perlin/SimuEnginePerlin";
+import { ToolbarPerlin } from "./perlin/ToolbarPerlin";
+import { SimuEngineSort } from "./sort/SimuEngineSort";
+import { ToolbarSort } from "./sort/ToolbarSort";
+import { SimuEngineTSP } from "./tsp/SimuEngineTSP";
+import { ToolbarTSP } from "./tsp/ToolbarTSP";
 
 interface SimulationPopupProps {
     selectedSimulation: string;
@@ -15,14 +31,6 @@ function SimulationPopup({ selectedSimulation, closePopup }: SimulationPopupProp
     const [simuEngine, setSimuEngine] = useState<SimuEngine | null>(null);
     const [simuToolbar, setSimuToolbar] = useState<Toolbar | null>(null);
 
-    const getSimuEngineCallBack = (childEngine: SimuEngine) => {
-        setSimuEngine(childEngine);
-    }
-
-    const getSimuToolbarCallBack = (childToolbar: Toolbar) => {
-        setSimuToolbar(childToolbar);
-    }
- 
     const close = () => {
         closePopup()
 		// Stop the simulation when the pop-up is closed
@@ -31,23 +39,110 @@ function SimulationPopup({ selectedSimulation, closePopup }: SimulationPopupProp
 		}
 	};
 
+    const canvasRef = useRef<HTMLCanvasElement>(null);
+
+    useEffect(() => {
+        const canvas: HTMLCanvasElement = canvasRef.current!;
+        const context: CanvasRenderingContext2D = canvas.getContext('2d')!;
+        const container: HTMLElement = document.getElementById("CanvasContainer")!;
+        const diagLength = adaptCanvas(canvas, container);
+        let newSimuEngine: SimuEngine;
+        let newSimuToolbar: Toolbar;
+    
+        if (selectedSimulation === 'Boids Simulation') {
+    
+            newSimuEngine = new SimuEngineBoids(canvas, context, diagLength);
+            newSimuToolbar = new ToolbarBoids(newSimuEngine);
+    
+        } else if (selectedSimulation === 'Sorting algorithms') {
+    
+            newSimuEngine = new SimuEngineSort(canvas, context, diagLength);
+            newSimuToolbar = new ToolbarSort(newSimuEngine);
+        }
+        else if (selectedSimulation === 'Shortest Paths') {
+    
+            newSimuEngine = new SimuEnginePaths(canvas, context, diagLength);
+            newSimuToolbar = new ToolbarPaths(newSimuEngine);
+    
+        }
+        else if (selectedSimulation === 'TSP') {
+    
+            newSimuEngine = new SimuEngineTSP(canvas, context, diagLength);
+            newSimuToolbar = new ToolbarTSP(newSimuEngine);
+    
+        }
+        else if (selectedSimulation === "Conway's Game of Life") {
+    
+            newSimuEngine = new SimuEngineGOL(canvas, context, diagLength);
+            newSimuToolbar = new ToolbarGOL(newSimuEngine);
+    
+        }
+        else if (selectedSimulation === "Fractal Simulation") {
+    
+            newSimuEngine = new SimuEngineFractal(canvas, context, diagLength);
+            newSimuToolbar = new ToolbarFractal(newSimuEngine);
+    
+        }
+        else if (selectedSimulation === "Perlin Noise") {
+    
+            newSimuEngine = new SimuEnginePerlin(canvas, context, diagLength);
+            newSimuToolbar = new ToolbarPerlin(newSimuEngine);
+    
+        }
+        else if (selectedSimulation === "Blob Simulation") {
+    
+            newSimuEngine = new SimuEngineDLA(canvas, context, diagLength);
+            newSimuToolbar = new ToolbarDLA(newSimuEngine);
+    
+        }
+        else {
+            newSimuEngine = new SimuEngineBoids(canvas, context, diagLength);
+            newSimuToolbar = new ToolbarBoids(newSimuEngine);
+        }
+    
+        setSimuEngine(newSimuEngine);
+        setSimuToolbar(newSimuToolbar);
+
+        return () => {
+            newSimuEngine.stopLoop();
+        }
+
+    }, [selectedSimulation]);
+
     return (
         <Modal show={!!selectedSimulation} centered className="modal-xl">
             <Modal.Header closeButton onClick={close} style={{ backgroundColor: '#555', color: 'antiquewhite' }}>
                 <Modal.Title>{selectedSimulation}</Modal.Title>
             </Modal.Header>
             <Modal.Body id="CanvasContainer" style={{ backgroundColor: '#555', color: 'antiquewhite' }}>
-                <SimulationCanvas
-					selectedSimulation={selectedSimulation}
-                    getSimuEngineCallBack = {getSimuEngineCallBack}
-                    getSimuToolbarCallBack = {getSimuToolbarCallBack}
-				/>
+                <canvas ref={canvasRef} style={{height: '100%', width: '100%', flex: '1', objectPosition: 'center'}}></canvas>
             </Modal.Body>
             <Modal.Footer style={{ backgroundColor: '#555', color: 'antiquewhite' }}>
                 {simuToolbar && simuToolbar.getToolbar()}
             </Modal.Footer>
         </Modal>
     );
+}
+
+function adaptCanvas(canvas: HTMLCanvasElement, canvasContainer: HTMLElement): number {
+
+    const containerWidth = canvasContainer.clientWidth;
+    const containerHeight = canvasContainer.clientHeight;
+
+    const aspectRatio = canvas.width / canvas.height;
+    if (containerWidth / aspectRatio <= containerHeight) {
+        // Fit based on width
+        canvas.width = containerWidth;
+        canvas.height = containerWidth / aspectRatio;
+    } else {
+        // Fit based on height
+        canvas.height = containerHeight;
+        canvas.width = containerHeight * aspectRatio;
+    }
+
+    const diag = Math.sqrt((canvas.height * canvas.height) + (canvas.width * canvas.width));
+
+    return diag;
 }
 
 export default SimulationPopup;
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
@@ -11,11 +10,9 @@ const root = ReactDOM.createRoot(
 );
 
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
